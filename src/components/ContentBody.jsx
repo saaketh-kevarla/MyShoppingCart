60c6092f4cf4d187c28a67582ee1fd9e743fbc80
Saaketh Kevarla <saaketh.kevarla@erpa.com>
2025-06-27 16:35:01 -0400

    Work over Weekend

---
+++ src/components/ContentBody.jsx
@@ -0,0 +1,13 @@
+import {styled} from 'styled-components'
+
+const StyleDiv = styled.div`
+
+`
+
+export default function ContentBody(){
+    return (
+        <>
+             
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import {styled} from 'styled-components'
+
+const StyleDiv = styled.div`
+display : flex;
+border : 1px solid black;
+justify-content : space-between;
+align-items : center;
+
+
+& button {
+    height : 50px;
+    width : 100px;
+    font-size : 20px;
+    margin-right : 7vw;
+}
+`
+
+
+export default function Header(){
+    
+    return (
+        <>
+            <StyleDiv>
+                <h1>ELEGANT CONTENT</h1>
+                <button>Cart(0)</button>
+            </StyleDiv>
+        </>
+    )
+}
